fbc2476b96dd215f5d3adda0219c9f9d1b2688ef
aakash819 <aakash.goel462@gmail.com>
2019-07-22 15:44:43 +0530

    add routing for all trash post

--- src/app/modules/cms/modules/post/post-routing.module.ts
+++ src/app/modules/cms/modules/post/post-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AddNewPostComponent } from './components/add-new-post/add-new-post.component';
 import { AllPostsComponent } from './components/all-posts/all-posts.component';
 import { ManagePostCategoryComponent } from './components/manage-post-category/manage-post-category.component';
+import { AllTrashPostComponent } from './components/all-trash-post/all-trash-post.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'all-news',
     component: AllPostsComponent
+  },
+  {
+    path: 'all-trash',
+    component: AllTrashPostComponent
   },
   {
     path: 'manage-categories',
