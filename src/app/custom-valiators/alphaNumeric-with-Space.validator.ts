020e3917117877c163b10e4cd11d4047affb5796
aakash819 <aakash.goel462@gmail.com>
2019-07-22 15:34:12 +0530

    add custom validators for alpha numaric  with space

---
+++ src/app/custom-valiators/alphaNumeric-with-Space.validator.ts
@@ -0,0 +1,9 @@
+import { AbstractControl, Validator } from '@angular/forms';
+export function alphaNumericWithSpace(control: AbstractControl) {
+
+    const value = control.value.trim();
+    if (!/^[a-zA-Z0-9\s]+$/.test(value) && value) {
+        return { isAlphaNumeric: true };
+    }
+    return null;
+}
