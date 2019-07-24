79753af943e3a3c8bb0c26610273bd6644427534
aakash819 <aakash.goel462@gmail.com>
2019-07-24 17:29:22 +0530

    allow hyphen in case of slug

--- src/app/custom-valiators/alphaNumeric-with-Space.validator.ts
+++ src/app/custom-valiators/alphaNumeric-with-Space.validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl, Validator } from '@angular/forms';
 export function alphaNumericWithSpace(control: AbstractControl) {
 
     const value = control.value.trim();
-    if (!/^[a-zA-Z0-9\s]+$/.test(value) && value) {
+    if (!/^[a-zA-Z0-9\-_\s]+$/.test(value) && value) {
         return { isAlphaNumeric: true };
     }
     return null;
