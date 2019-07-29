5079afe11b91fcb6db16187dd96e649b374202a9
aakash819 <aakash.goel462@gmail.com>
2019-07-29 11:31:53 +0530

    add routing for menu modul

---
+++ src/app/modules/cms/modules/menu/menu-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddMenuComponent } from './components/add-menu/add-menu.component';
+import { AllMenuComponent } from './components/all-menu/all-menu.component';
+
+const routes: Routes = [
+  {
+    path: 'add-new-menu',
+    component: AddMenuComponent
+  },
+  {
+    path: 'all-menu',
+    component: AllMenuComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MenuRoutingModule { }
