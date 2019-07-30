69dfa0a718605409cd3da5c1cbaa3fb0e02a5071
aakash819 <aakash.goel462@gmail.com>
2019-07-30 18:08:35 +0530

    add activated router service for get id

--- src/app/modules/cms/modules/menu/components/add-menu/add-menu.component.ts
+++ src/app/modules/cms/modules/menu/components/add-menu/add-menu.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from 'src/app/service/menu/menu.service';
+import { ActivatedRoute } from '@angular/router';
+import { MenuModel } from 'src/app/model/menu.model';
+import { PostService } from 'src/app/service/post/post.service';
+import { PostCategoryModel } from 'src/app/model/post-category.model';
+import { PostModel } from 'src/app/model/post.model';
+import { MatCheckboxChange } from '@angular/material';
 
 @Component({
   selector: 'app-add-menu',
@@ -6,10 +13,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-menu.component.scss']
 })
 export class AddMenuComponent implements OnInit {
+  public menuId: number = null;
+  public errMsg: string = '';
+  public menu: MenuModel = null;
+  public showLoader: boolean = true;
+  public categoryList: PostCategoryModel[] = [];
+  public postList: PostModel[] = [];
 
-  constructor() { }
+
+  constructor(
+    private menuSerive: MenuService,
+    private activatedRoute: ActivatedRoute,
+    private postSerive: PostService
+  ) {
+    this.menuId = parseInt(this.activatedRoute.snapshot.paramMap.get("id"));
+
+  }
+
 
   ngOnInit() {
+
+    setTimeout(() => {
+      this.showLoader = false;
+    }, 1000);
   }
+
+
 
 }
