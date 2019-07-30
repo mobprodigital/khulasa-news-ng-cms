06913ea1e86547e3bf9eda9f0f666f18770518c2
aakash819 <aakash.goel462@gmail.com>
2019-07-30 17:52:42 +0530

    modify menu model and set default value

--- src/app/model/menu.model.ts
+++ src/app/model/menu.model.ts
@@ -1,30 +1,30 @@
+import { MenuItemTypeEnum } from '../enum/menu-item-type.enum';
 
 /**
  * menu model for Portal view
  */
 export class MenuModel {
     /**Id of menu*/
-    public menuId: number;
+    public menuId: number = null;
     /**name of menu */
-    public menuName: string;
+    public menuName: string = '';
     /**array of menuItem */
-    public menuItems: MenuItemModel[];
+    public menuItems: MenuItemModel[] = [];
 }
 
 
 /** 
 * menu item model for menuItems
 */
-
 export class MenuItemModel {
     /**id of menuItem */
-    public menuItemId: number;
+    public itemId: number = null;
     /**name of menuItem */
-    public menuItemName: string;
+    public itemName: string = '';
     /**type of menuItem */
-    public menuItemType: string;
+    public itemType: MenuItemTypeEnum = null;
     /**url of menuItem */
-    public menuItemUrl: string;
+    public itemUrl: string = '';
     /**target of menuItem */
-    public target: string;
+    public target: string = '';
 }
